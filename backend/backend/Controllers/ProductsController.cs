a5684b35c9df46e950b7602ccd69edb1fb4f96ae
AP <aleksandrpolishchuk@gmail.com>
2023-05-25 23:58:06 +0700

    add ProductsController

---
+++ backend/backend/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using backend.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ProductsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		// CRUD -> Create - Read - Update - Delete
+
+		// Create
+
+		// Read
+
+		// Update
+
+		// Delete
+	}
+}
