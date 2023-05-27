257481c9581627b9bde2da4c740a0d0efac11d63
AP <aleksandrpolishchuk@gmail.com>
2023-05-27 23:51:24 +0700

    add minimal structure of components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      {/* Navbar */}
+      {/* <NavBar /> */}
+
+      {/* Wrapper */}
+      <div className="wrapper">{/* // Routes */}</div>
+    </div>
+  );
 };
 
 export default App;
