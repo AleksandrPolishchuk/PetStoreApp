65929ff56fca735c3df36cc455e532b15e758ad2
AP <aleksandrpolishchuk@gmail.com>
2023-05-30 23:57:10 +0700

    add assets

--- frontend/src/pages/home/Home.page.tsx
+++ frontend/src/pages/home/Home.page.tsx
@@ -1,7 +1,11 @@
 import "./home.scss";
 
 const Home = () => {
-  return <div>Home.page</div>;
+  return (
+    <div className="home">
+      <h1>Wetlome to Stores</h1>
+    </div>
+  );
 };
 
 export default Home;
