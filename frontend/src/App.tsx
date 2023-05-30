cf18803780c2fe8d8023e64afef529a060f52d93
AP <aleksandrpolishchuk@gmail.com>
2023-05-30 23:54:55 +0700

    add routes for App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home.page";
 
 const App = () => {
   return (
@@ -8,7 +10,12 @@ const App = () => {
       <Navbar />
 
       {/* Wrapper */}
-      <div className="wrapper">{/* // Routes */}</div>
+      <div className="wrapper">
+        {/* // Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
