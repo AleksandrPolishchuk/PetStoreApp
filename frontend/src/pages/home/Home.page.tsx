fe0a547ba12ead7c6bf4c40a2ad96338690e5b43
AP <aleksandrpolishchuk@gmail.com>
2023-05-31 22:57:32 +0700

    add useNavigate for Home.page

--- frontend/src/pages/home/Home.page.tsx
+++ frontend/src/pages/home/Home.page.tsx
@@ -1,11 +1,17 @@
 import "./home.scss";
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const redirect = useNavigate();
   return (
     <div className="home">
       <h1>Wetlome to Store</h1>
-      <Button variant="outlined" color="primary">
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={() => redirect("/products")}
+      >
         Product List
       </Button>
     </div>
