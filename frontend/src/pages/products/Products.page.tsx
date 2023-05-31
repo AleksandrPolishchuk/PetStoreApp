7a5001e26302fff015cccf1f0319b3e66e87ac1b
AP <aleksandrpolishchuk@gmail.com>
2023-05-31 23:54:27 +0700

    add global.typing

--- frontend/src/pages/products/Products.page.tsx
+++ frontend/src/pages/products/Products.page.tsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from "react";
 import "./products.scss";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
   return (
     <div className="products">
       <h1>Products List</h1>
