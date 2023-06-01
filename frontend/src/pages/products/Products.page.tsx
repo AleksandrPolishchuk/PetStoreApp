110f4c6922962abf42cd897eb53a9048a897c3d3
AP <aleksandrpolishchuk@gmail.com>
2023-06-01 23:50:35 +0700

    add constants

--- frontend/src/pages/products/Products.page.tsx
+++ frontend/src/pages/products/Products.page.tsx
@@ -1,9 +1,18 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import "./products.scss";
 import { IProduct } from "../../types/global.typing";
+import { baseUrl } from "../../constants/url.constant";
 
 const Products = () => {
   const [products, setProducts] = useState<IProduct[]>([]);
+
+  const fetchProductsList = async () => {
+    try {
+      const response = await axios.get<IProduct[]>(baseUrl);
+    } catch (error) {}
+  };
+
   return (
     <div className="products">
       <h1>Products List</h1>
