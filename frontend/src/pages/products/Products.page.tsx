eea13a5c733c6f3298e3aa34f34691d92ccb77a0
AP <aleksandrpolishchuk@gmail.com>
2023-06-03 23:53:17 +0700

    add mui/icons-material

--- frontend/src/pages/products/Products.page.tsx
+++ frontend/src/pages/products/Products.page.tsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import "./products.scss";
 import { IProduct } from "../../types/global.typing";
 import { baseUrl } from "../../constants/url.constant";
+import { Button } from "@mui/material";
+import { Edit, Delete } from "@mui/icons-material";
 
 const Products = () => {
   const [products, setProducts] = useState<IProduct[]>([]);
@@ -20,11 +22,46 @@ const Products = () => {
     fetchProductsList();
   }, []);
 
-  console.log(products);
+  // console.log(products);
 
   return (
     <div className="products">
       <h1>Products List</h1>
+      {products.length === 0 ? (
+        <h1>No Products</h1>
+      ) : (
+        <div className="table-wrapper">
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Brand</th>
+                <th>Creation Time</th>
+                <th>Update Time</th>
+                <th>Operations</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr>
+                  <td>{product.title}</td>
+                  <td>{product.brand}</td>
+                  <td>{product.createdAt}</td>
+                  <td>{product.updatedAt}</td>
+                  <td>
+                    <Button variant="outlined" color="warning" sx={{ mx: 3 }}>
+                      <Edit />
+                    </Button>
+                    <Button variant="outlined" color="error">
+                      <Delete />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
