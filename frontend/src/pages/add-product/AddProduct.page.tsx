16c061f63e9a3201b1565af72e2a82a4cbec333e
AP <aleksandrpolishchuk@gmail.com>
2023-06-05 23:53:02 +0700

    add html&css for AddProduct.page

--- frontend/src/pages/add-product/AddProduct.page.tsx
+++ frontend/src/pages/add-product/AddProduct.page.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./add-product.scss";
+import { TextField, Button } from "@mui/material";
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  return (
+    <div className="add-product">
+      <h2>Add New Product</h2>
+      <TextField autoComplete="off" label="Brand" variant="outlined" />
+      <TextField autoComplete="off" label="Title" variant="outlined" />
+      <Button variant="outlined" color="primary">
+        Save
+      </Button>
+      <Button variant="outlined" color="secondary">
+        Back
+      </Button>
+    </div>
+  );
 };
 
 export default AddProduct;
