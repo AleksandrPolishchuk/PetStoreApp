30e5c2f3330fcc6eacb4e73e8d44370fd7b22f28
AP <aleksandrpolishchuk@gmail.com>
2023-06-05 23:58:18 +0700

    add useState for AddProduct.page

--- frontend/src/pages/add-product/AddProduct.page.tsx
+++ frontend/src/pages/add-product/AddProduct.page.tsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./add-product.scss";
 import { TextField, Button } from "@mui/material";
+import { IProduct } from "../../types/global.typing";
 
 const AddProduct = () => {
+  const [product, setProduct] = useState<Partial<IProduct>>({
+    title: "",
+    brand: "",
+  });
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setProduct({
+      ...product,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <div className="add-product">
       <h2>Add New Product</h2>
-      <TextField autoComplete="off" label="Brand" variant="outlined" />
-      <TextField autoComplete="off" label="Title" variant="outlined" />
+      <TextField
+        autoComplete="off"
+        label="Brand"
+        variant="outlined"
+        value={product.brand}
+        onChange={changeHandler}
+      />
+      <TextField
+        autoComplete="off"
+        label="Title"
+        variant="outlined"
+        value={product.title}
+        onChange={changeHandler}
+      />
       <Button variant="outlined" color="primary">
         Save
       </Button>
